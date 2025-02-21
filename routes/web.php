3d24c98b4d2ecc1f7878cac4c8a0999ba8b06fed
AlbertWebs <albertmuhatia@gmail.com>
2025-02-21 15:46:49 +0300

    Search Routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/mobile/get-started', [App\Http\Controllers\MobileController::class, 'index'])->name('get-started');
+Route::get('/search', [App\Http\Controllers\MobileController::class, 'search'])->name('search');
+Route::post('/search-post', [App\Http\Controllers\MobileController::class, 'search_post'])->name('search_post');
