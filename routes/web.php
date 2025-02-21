bb0968d5c9fc8dd24f0c666244f4637da3d55014
AlbertWebs <albertmuhatia@gmail.com>
2025-02-21 16:01:36 +0300

    update route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::group(['prefix'=>'mobile'], function(){
     Route::get('/get-started', [App\Http\Controllers\MobileController::class, 'index'])->name('get-started');
     Route::get('/search', [App\Http\Controllers\MobileController::class, 'search'])->name('search');
     Route::post('/search-post', [App\Http\Controllers\MobileController::class, 'search_post'])->name('search_post');
+
+    Route::post('/sign-up', [App\Http\Controllers\MobileLoginController::class, 'sign_up_post'])->name('mobile.signup.post');
+    Route::get('/sign-in', [App\Http\Controllers\MobileLoginController::class, 'sign_in'])->name('sign-in');
+    Route::get('/sign-up', [App\Http\Controllers\MobileLoginController::class, 'sign_up'])->name('mobile.sign-up');
+    Route::get('/forgot-password', [App\Http\Controllers\MobileLoginController::class, 'forgot_password'])->name('mobile.forgot-password');
+    Route::get('/email-success', [App\Http\Controllers\MobileLoginController::class, 'email_success'])->name('mobile.email-success');
+    Route::get('/logout', [App\Http\Controllers\MobileLoginController::class, 'logouts']);
 });
