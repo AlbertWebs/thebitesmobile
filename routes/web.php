7d6fb3952f374e82218fd0b389dff002ac7e2a98
AlbertWebs <albertmuhatia@gmail.com>
2025-02-21 16:24:25 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,18 @@ Route::group(['prefix'=>'mobile'], function(){
     Route::get('/search', [App\Http\Controllers\MobileController::class, 'search'])->name('search');
     Route::post('/search-post', [App\Http\Controllers\MobileController::class, 'search_post'])->name('search_post');
 
+    Route::group(['prefix'=>'profile'], function(){
+        Route::get('/', [App\Http\Controllers\MobileController::class, 'profile'])->name('profile');
+        Route::get('/edit-profile', [App\Http\Controllers\MobileController::class, 'edit_profile'])->name('edit-profile');
+        Route::post('/edit-profile-post', [App\Http\Controllers\MobileController::class, 'edit_profile_post'])->name('edit-profile-post');
+        Route::get('/transactions', [App\Http\Controllers\MobileController::class, 'transactions'])->name('transactions');
+        Route::get('/orders', [App\Http\Controllers\MobileController::class, 'orders'])->name('orders');
+        Route::get('/orders/place-orders', [App\Http\Controllers\MobileController::class, 'place_orders'])->name('place-orders');
+        Route::get('/orders/{id}', [App\Http\Controllers\MobileController::class, 'orders_details'])->name('orders-details');
+        Route::get('/orders/re-order/{id}', [App\Http\Controllers\MobileController::class, 'orders_re_order'])->name('orders-re-order');
+        Route::get('/orders/mailClient', [App\Http\Controllers\MobileController::class, 'mailClient'])->name('orders-mailClient');
+    });
+
     //Auths
     Route::post('/sign-up', [App\Http\Controllers\MobileLoginController::class, 'sign_up_post'])->name('mobile.signup.post');
     Route::get('/sign-in', [App\Http\Controllers\MobileLoginController::class, 'sign_in'])->name('sign-in');
